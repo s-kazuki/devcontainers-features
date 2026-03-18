2b9a1d110f9786886f1cce3f2d57863eafabf2df
s-kazuki <sugiyama.kazuki@revoneo.com>
2026-03-18 08:39:08 +0000

    feat: pass env

--- src/coder-login/install.sh
+++ src/coder-login/install.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 set -e
 
-CODER_URL=${CODERURL:-""}
-CODER_SESSION_TOKEN=${CODERSESSIONTOKEN:-""}
-
 if ! command -v coder >/dev/null 2>&1; then
     echo "Coder CLI could not be found. Please ensure it is installed by another feature."
     exit 1
 fi
+
+CODER_ENV_PATH=/etc/profile.d/coder-env.sh
+
+> "$CODER_ENV_PATH"
+chmod +x "$CODER_ENV_PATH"
+
+env | grep '^CODER' | while IFS='=' read -r key value; do
+  echo "export ${key}='${value}'" >> "$CODER_ENV_PATH"
+done
+env | grep '^GIT' | while IFS='=' read -r key value; do
+  echo "export ${key}='${value}'" >> "$CODER_ENV_PATH"
+done
 
 coder login --url=${CODER_URL} --token=${CODER_SESSION_TOKEN}
